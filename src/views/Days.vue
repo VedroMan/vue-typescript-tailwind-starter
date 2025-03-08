58531d0cbbaf820741b995b4d55cc901bd90a991
VedroMan <123834638+VedroMan@users.noreply.github.com>
2025-03-09 01:54:57 +0300

    Добавлен footer ко всем views

--- src/views/Days.vue
+++ src/views/Days.vue
@@ -1,12 +1,18 @@
 <template>
-  <div>
-    <h1 class="text-gray-900 white:text-dark mt-5 text-base font-bold tracking-tight ">Дни недели 📆</h1>
-    <ul>
-      <li v-for="day in days" :key="day.id">
+  <div class="flex flex-col min-h-screen">
+    <main class="grow">
+      <h1 class="text-gray-900 white:text-dark mt-5 text-base font-bold tracking-tight ">Дни недели 📆</h1>
+      <ul>
+        <li v-for="day in days" :key="day.id">
         {{ day.day }}
-      </li>
-    </ul>
+        </li>
+      </ul>
+    </main>
   </div>
+
+  <footer class="text-gray-400 white:text-dark mt-10 mb-10 text-xs py-4 font-medium tracking-tight text-center">
+    <p>Produced by Tim Zykov</p>
+  </footer>
 </template>
 
 <script setup lang="ts">
--- src/views/Teachers.vue
+++ src/views/Teachers.vue
@@ -1,12 +1,18 @@
 <template>
-  <div>
-    <h1 class="text-gray-900 white:text-dark mt-5 text-base font-bold tracking-tight ">Учителя 👩‍🏫</h1>
-    <ul>
-      <li v-for="teacher in teachers" :key="teacher.id">
+  <div class="flex flex-col min-h-screen">
+    <main class="grow">
+      <h1 class="text-gray-900 white:text-dark mt-5 text-base font-bold tracking-tight ">Учителя 👩‍🏫</h1>
+      <ul>
+        <li v-for="teacher in teachers" :key="teacher.id">
         {{ teacher.name }}
-      </li>
-    </ul>
+        </li>
+      </ul>
+    </main>
   </div>
+
+  <footer class="text-gray-400 white:text-dark mt-10 mb-10 text-xs py-4 font-medium tracking-tight text-center">
+    <p>Produced by Tim Zykov</p>
+  </footer>
 </template>
 
 <script setup lang="ts">
